21f6efeeabc9d972c776056e79bbb9aebf0accad
pbbp0904 <helmerichchristopher0904@gmail.com>
2021-12-17 21:44:25 -0600

    added back plotting

--- HELENMain.m
+++ HELENMain.m
@@ -30,9 +30,9 @@ if exist('errors.xls', 'file') && 1
     %runNumber = length(xlsread('training.xlsx','Data'));
     [runNumber,~] = size(xlsread('errors.xls','Errors'));
     runNumber = runNumber - 1;
-    %errors = xlsread('errors.xls','Errors');
-    %scatter(handles.error,errors(:,6),errors(:,2),20,'filled','LineWidth',.05,'MarkerFaceColor',[0, 0, 1]);
-    %scatter(handles.error2,errors(:,6),errors(:,3),20,'filled','LineWidth',.05,'MarkerFaceColor',[0, 0, 1]);
+    errors = xlsread('errors.xls','Errors');
+    scatter(handles.error,errors(:,6),errors(:,2),20,'filled','LineWidth',.05,'MarkerFaceColor',[0, 0, 1]);
+    scatter(handles.error2,errors(:,6),errors(:,3),20,'filled','LineWidth',.05,'MarkerFaceColor',[0, 0, 1]);
 else
     runNumber = 0;
 end
@@ -63,10 +63,10 @@ global payloadTimings;
 
 timingMultiplier = 1;
 
-%cla(handles.mainPlot,'reset');
-%rotate3d on
+cla(handles.mainPlot,'reset');
+rotate3d on
 
-if(fileRead==1&&0)
+if(fileRead==1)
     payloads=xlsread(fileName,'Payloads');
     tgf=xlsread(fileName,'TGF');
     timing=double(xlsread(fileName,'Timing'));
@@ -80,20 +80,20 @@ else
     visual = [5, 10];
 end
 
-%[x,y,z] = sphere;
-%scale = visual(1,1);
+[x,y,z] = sphere;
+scale = visual(1,1);
 
-%hold(handles.mainPlot,'on')
-%surf(handles.mainPlot,x/scale+payloads(1,1),y/scale+payloads(1,2),z/scale+payloads(1,3),'EdgeColor','blue');
-%surf(handles.mainPlot,x/scale+payloads(2,1),y/scale+payloads(2,2),z/scale+payloads(2,3),'EdgeColor','blue');
-%surf(handles.mainPlot,x/scale+payloads(3,1),y/scale+payloads(3,2),z/scale+payloads(3,3),'EdgeColor','blue');
-%surf(handles.mainPlot,x/scale+payloads(4,1),y/scale+payloads(4,2),z/scale+payloads(4,3),'EdgeColor','blue');
+hold(handles.mainPlot,'on')
+surf(handles.mainPlot,x/scale+payloads(1,1),y/scale+payloads(1,2),z/scale+payloads(1,3),'EdgeColor','blue');
+surf(handles.mainPlot,x/scale+payloads(2,1),y/scale+payloads(2,2),z/scale+payloads(2,3),'EdgeColor','blue');
+surf(handles.mainPlot,x/scale+payloads(3,1),y/scale+payloads(3,2),z/scale+payloads(3,3),'EdgeColor','blue');
+surf(handles.mainPlot,x/scale+payloads(4,1),y/scale+payloads(4,2),z/scale+payloads(4,3),'EdgeColor','blue');
 
-%set(handles.mainPlot,'XLim',[min(min(payloads(:,1)),tgf(1,1))-.4,max(max(payloads(:,1)),tgf(1,1))+.4]);
-%set(handles.mainPlot,'YLim',[min(min(payloads(:,2)),tgf(1,2))-.4,max(max(payloads(:,2)),tgf(1,2))+.4]);
-%set(handles.mainPlot,'ZLim',[min(min(payloads(:,3)),tgf(1,3))-.4,max(max(payloads(:,3)),tgf(1,3))+.4]);
+set(handles.mainPlot,'XLim',[min(min(payloads(:,1)),tgf(1,1))-.4,max(max(payloads(:,1)),tgf(1,1))+.4]);
+set(handles.mainPlot,'YLim',[min(min(payloads(:,2)),tgf(1,2))-.4,max(max(payloads(:,2)),tgf(1,2))+.4]);
+set(handles.mainPlot,'ZLim',[min(min(payloads(:,3)),tgf(1,3))-.4,max(max(payloads(:,3)),tgf(1,3))+.4]);
 
-%surf(handles.mainPlot,x/scale+tgf(1,1),y/scale+tgf(1,2),z/scale+tgf(1,3),'EdgeColor','green');
+surf(handles.mainPlot,x/scale+tgf(1,1),y/scale+tgf(1,2),z/scale+tgf(1,3),'EdgeColor','green');
 
 %payloads
 
@@ -118,9 +118,9 @@ p4DistanceFromTGF = sqrt((payloads(4,1)-tgf(1,1))^2+(payloads(4,2)-tgf(1,2))^2+(
 maxDistance = max(max(max(p1DistanceFromTGF,p2DistanceFromTGF),p3DistanceFromTGF),p4DistanceFromTGF)+.2;
 maxTime = maxDistance/(299792.458);
 
-%set(handles.mainPlot,'XLim',[min(min(payloads(:,1)),tgf(1,1))-.4,max(max(payloads(:,1)),tgf(1,1))+.4]);
-%set(handles.mainPlot,'YLim',[min(min(payloads(:,2)),tgf(1,2))-.4,max(max(payloads(:,2)),tgf(1,2))+.4]);
-%set(handles.mainPlot,'ZLim',[min(min(payloads(:,3)),tgf(1,3))-.4,max(max(payloads(:,3)),tgf(1,3))+.4]);
+set(handles.mainPlot,'XLim',[min(min(payloads(:,1)),tgf(1,1))-.4,max(max(payloads(:,1)),tgf(1,1))+.4]);
+set(handles.mainPlot,'YLim',[min(min(payloads(:,2)),tgf(1,2))-.4,max(max(payloads(:,2)),tgf(1,2))+.4]);
+set(handles.mainPlot,'ZLim',[min(min(payloads(:,3)),tgf(1,3))-.4,max(max(payloads(:,3)),tgf(1,3))+.4]);
 
 time = 0;
 iteration = 0;
@@ -133,71 +133,71 @@ while time < maxTime
     %Check if payload is detecting TGF
     if(~p1 && distance>p1DistanceFromTGF)
         p1Time = (time+(time+timeStep))/2;
-        %set(handles.time1,'String',strcat('Detection Time 1:',{'     '},num2str(p1Time*10^9,10)));
+        set(handles.time1,'String',strcat('Detection Time 1:',{'     '},num2str(p1Time*10^9,10)));
         p1 = 1;
     end
     if(~p2 && distance>p2DistanceFromTGF)
         p2Time = (time+(time+timeStep))/2;
-        %set(handles.time2,'String',strcat('Detection Time 2:',{'     '},num2str(p2Time*10^9,10)));
+        set(handles.time2,'String',strcat('Detection Time 2:',{'     '},num2str(p2Time*10^9,10)));
         p2 = 1;
     end
     if(~p3 && distance>p3DistanceFromTGF)
         p3Time = (time+(time+timeStep))/2;
-        %set(handles.time3,'String',strcat('Detection Time 3:',{'     '},num2str(p3Time*10^9,10)));
+        set(handles.time3,'String',strcat('Detection Time 3:',{'     '},num2str(p3Time*10^9,10)));
         p3 = 1;
     end
     if(~p4 && distance>p4DistanceFromTGF)
         p4Time = (time+(time+timeStep))/2;
-        %set(handles.time4,'String',strcat('Detection Time 4:',{'     '},num2str(p4Time*10^9,10)));
+        set(handles.time4,'String',strcat('Detection Time 4:',{'     '},num2str(p4Time*10^9,10)));
         p4 = 1;
     end
     
     %Plotting
-    %if(mod(iteration,floor((maxTime/timeStep)/visual(1,2)))==0)
-        %hold(handles.mainPlot,'off')
-        %surf(handles.mainPlot,x*distance+tgf(1,1),y*distance+tgf(1,2),z*distance+tgf(1,3),'EdgeColor','yellow','FaceAlpha',.05);
-        %hold(handles.mainPlot,'on')
-        %surf(handles.mainPlot,x/scale+payloads(1,1),y/scale+payloads(1,2),z/scale+payloads(1,3),'EdgeColor','blue');
-        %surf(handles.mainPlot,x/scale+payloads(2,1),y/scale+payloads(2,2),z/scale+payloads(2,3),'EdgeColor','blue');
-        %surf(handles.mainPlot,x/scale+payloads(3,1),y/scale+payloads(3,2),z/scale+payloads(3,3),'EdgeColor','blue');
-        %surf(handles.mainPlot,x/scale+payloads(4,1),y/scale+payloads(4,2),z/scale+payloads(4,3),'EdgeColor','blue');
-        %drawnow
-    %end
+    if(mod(iteration,floor((maxTime/timeStep)/visual(1,2)))==0)
+        hold(handles.mainPlot,'off')
+        surf(handles.mainPlot,x*distance+tgf(1,1),y*distance+tgf(1,2),z*distance+tgf(1,3),'EdgeColor','yellow','FaceAlpha',.05);
+        hold(handles.mainPlot,'on')
+        surf(handles.mainPlot,x/scale+payloads(1,1),y/scale+payloads(1,2),z/scale+payloads(1,3),'EdgeColor','blue');
+        surf(handles.mainPlot,x/scale+payloads(2,1),y/scale+payloads(2,2),z/scale+payloads(2,3),'EdgeColor','blue');
+        surf(handles.mainPlot,x/scale+payloads(3,1),y/scale+payloads(3,2),z/scale+payloads(3,3),'EdgeColor','blue');
+        surf(handles.mainPlot,x/scale+payloads(4,1),y/scale+payloads(4,2),z/scale+payloads(4,3),'EdgeColor','blue');
+        drawnow
+    end
     
     time=time+timeStep;
     iteration=iteration+1;
 end
 
-%plot3(handles.mainPlot,[tgf(1,1) payloads(1,1)],[tgf(1,2) payloads(1,2)],[tgf(1,3),payloads(1,3)],'k-','LineWidth',1,'Color',[1 0 0]);
-%plot3(handles.mainPlot,[tgf(1,1) payloads(2,1)],[tgf(1,2) payloads(2,2)],[tgf(1,3),payloads(2,3)],'k-','LineWidth',1,'Color',[1 0 0]);
-%plot3(handles.mainPlot,[tgf(1,1) payloads(3,1)],[tgf(1,2) payloads(3,2)],[tgf(1,3),payloads(3,3)],'k-','LineWidth',1,'Color',[1 0 0]);
-%plot3(handles.mainPlot,[tgf(1,1) payloads(4,1)],[tgf(1,2) payloads(4,2)],[tgf(1,3),payloads(4,3)],'k-','LineWidth',1,'Color',[1 0 0]);
+plot3(handles.mainPlot,[tgf(1,1) payloads(1,1)],[tgf(1,2) payloads(1,2)],[tgf(1,3),payloads(1,3)],'k-','LineWidth',1,'Color',[1 0 0]);
+plot3(handles.mainPlot,[tgf(1,1) payloads(2,1)],[tgf(1,2) payloads(2,2)],[tgf(1,3),payloads(2,3)],'k-','LineWidth',1,'Color',[1 0 0]);
+plot3(handles.mainPlot,[tgf(1,1) payloads(3,1)],[tgf(1,2) payloads(3,2)],[tgf(1,3),payloads(3,3)],'k-','LineWidth',1,'Color',[1 0 0]);
+plot3(handles.mainPlot,[tgf(1,1) payloads(4,1)],[tgf(1,2) payloads(4,2)],[tgf(1,3),payloads(4,3)],'k-','LineWidth',1,'Color',[1 0 0]);
 
-%axes(handles.mainPlot);
-%imshow(imread('huntsville.jpg'), 'XData', [-45 45], 'YData', [-45 45]);
+axes(handles.mainPlot);
+imshow(imread('huntsville.jpg'), 'XData', [-45 45], 'YData', [-45 45]);
 
-%xlim(handles.mainPlot,[-45 45]);
-%ylim(handles.mainPlot,[-45 45]);
-%zlim(handles.mainPlot,[0 40]);
+xlim(handles.mainPlot,[-45 45]);
+ylim(handles.mainPlot,[-45 45]);
+zlim(handles.mainPlot,[0 40]);
 
-%surf(handles.mainPlot,x/(scale-2)+tgf(1,1),y/(scale-2)+tgf(1,2),z/(scale-2)+tgf(1,3),'EdgeColor','magenta');
-%drawnow
+surf(handles.mainPlot,x/(scale-2)+tgf(1,1),y/(scale-2)+tgf(1,2),z/(scale-2)+tgf(1,3),'EdgeColor','magenta');
+drawnow
 p1TimeMeasured = p1Time + payloadTimings(1);
 p2TimeMeasured = p2Time + payloadTimings(2);
 p3TimeMeasured = p3Time + payloadTimings(3);
 p4TimeMeasured = p4Time + payloadTimings(4);
 [x0,y0,z0,xp,yp,zp] = TGFSolve(payloads,[p1TimeMeasured,p2TimeMeasured,p3TimeMeasured,p4TimeMeasured],tgf);
 
-% hold(handles.mainPlot,'off')
-% surf(handles.mainPlot,x/scale+tgf(1,1),y/scale+tgf(1,2),z/scale+tgf(1,3),'EdgeColor','magenta');
-% hold(handles.mainPlot,'on')
+hold(handles.mainPlot,'off')
+surf(handles.mainPlot,x/scale+tgf(1,1),y/scale+tgf(1,2),z/scale+tgf(1,3),'EdgeColor','magenta');
+hold(handles.mainPlot,'on')
 
-% surf(handles.mainPlot,x/scale+payloads(1,1),y/scale+payloads(1,2),z/scale+payloads(1,3),'EdgeColor','blue');
-% surf(handles.mainPlot,x/scale+payloads(2,1),y/scale+payloads(2,2),z/scale+payloads(2,3),'EdgeColor','blue');
-% surf(handles.mainPlot,x/scale+payloads(3,1),y/scale+payloads(3,2),z/scale+payloads(3,3),'EdgeColor','blue');
-% surf(handles.mainPlot,x/scale+payloads(4,1),y/scale+payloads(4,2),z/scale+payloads(4,3),'EdgeColor','blue');
-% 
-% surf(handles.mainPlot,x/scale+x0,y/scale+y0,z/scale+z0,'EdgeColor','red');
+surf(handles.mainPlot,x/scale+payloads(1,1),y/scale+payloads(1,2),z/scale+payloads(1,3),'EdgeColor','blue');
+surf(handles.mainPlot,x/scale+payloads(2,1),y/scale+payloads(2,2),z/scale+payloads(2,3),'EdgeColor','blue');
+surf(handles.mainPlot,x/scale+payloads(3,1),y/scale+payloads(3,2),z/scale+payloads(3,3),'EdgeColor','blue');
+surf(handles.mainPlot,x/scale+payloads(4,1),y/scale+payloads(4,2),z/scale+payloads(4,3),'EdgeColor','blue');
+
+surf(handles.mainPlot,x/scale+x0,y/scale+y0,z/scale+z0,'EdgeColor','red');
 
 %drawnow
 errorx = sqrt((x0-tgf(1,1))^2)*1000;
@@ -210,7 +210,7 @@ writex = cat(2,[errorx,errory,errorz],sqrt((errorx)^2+(errory^2)+(errorz^2)),tim
 xlswrite('errors.xls',writex,'Errors',strcat('A',num2str(runNumber)));
 %writex = cat(2, payloads(1,:),payloads(2,:),payloads(3,:),payloads(4,:),[p1Time,p2Time,p3Time,p4Time],timing(1,1),tgf);
 %xlswrite('training.xlsx',writex,'Data',strcat('A',num2str(runNumber)));
-%scatter(handles.error,timing(1,1),sqrt((errorx)^2+(errory^2)+(errorz^2)),20,'filled','LineWidth',.05,'MarkerFaceColor',[0, 0, 1]);
+scatter(handles.error,timing(1,1),sqrt((errorx)^2+(errory^2)+(errorz^2)),20,'filled','LineWidth',.05,'MarkerFaceColor',[0, 0, 1]);
 fprintf('Run#: %i   X Error: %.5fm   Y Error: %.5fm   Z Error: %.5fm   Timing: %i \n', runNumber, errorx, errory, errorz, timing(1,1));
 fprintf('Run#: %i  XP Error: %.5fm  YP Error: %.5fm  ZP Error: %.5fm   Timing: %i \n', runNumber, errorxp, erroryp, errorzp, timing(1,1));
 %fprintf('Run#: %i\n', runNumber);
